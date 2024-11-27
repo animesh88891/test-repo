bffda6b573c22285b311a2b709e64d251145358a
animesh88891 <110978972+animesh88891@users.noreply.github.com>
2024-11-28 00:35:03 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "bucket_name" {
+  bucket = "devops-tf-state-ls"
+  lifecycle {
+    prevent_destroy = true
+  }
+  tags = {
+    "owner"   = "devops@animesh.in"
+    "Bu"      = "devops"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "s3_versioning" {
+  bucket = aws_s3_bucket.bucket_name.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags = {
+ 
+    "owner"   = "devops@animesh.in"
+    "Bu"      = "devops"
+  }
+}
